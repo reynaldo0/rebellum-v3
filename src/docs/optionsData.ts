06f837e51cd3e746676a5787367938e25fd2ac38
Feblcsack <alifianabdiel@gmail.com>
2024-09-24 10:31:03 +0700

    upd:(nambahin info)

--- src/docs/optionsData.ts
+++ src/docs/optionsData.ts
@@ -3,7 +3,7 @@ export const options = [
     {
         title: "Tawuran",
         description: "1-12 Bulan",
-        information: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        information: "Angka tawuran di Indonesia masih menjadi masalah serius, khususnya di kalangan pelajar. Pada tahun 2021, Badan Pusat Statistik (BPS) mencatat bahwa terdapat 188 desa/kelurahan di Indonesia yang menjadi lokasi tawuran pelajar. Provinsi Jawa Barat tercatat sebagai wilayah dengan angka tawuran pelajar tertinggi, yakni di 37 desa/kelurahan, diikuti oleh Sumatera Utara dan Maluku​",
         image: "/team/salman.jpg",
         backgroundColor: "#5562AD",
         height: "225px",
@@ -16,9 +16,9 @@ export const options = [
         ],
     },
     {
-        title: "Anak - Anak",
+        title: "Narkoba",
         description: "3-12 Tahun",
-        information: "loremipsumkan saja",
+        information: "Data terkait penyalahgunaan narkoba di Indonesia pada tahun 2023 menunjukkan bahwa Badan Narkotika Nasional (BNN) berhasil mengungkap 910 kasus narkotika dan menangkap 1.284 tersangka. Dari kasus tersebut, barang bukti terbesar yang disita termasuk 1,3 ton sabu, 1,4 ton ganja kering, dan ratusan ribu butir ekstasi. BNN juga menghancurkan 27,6 hektare ladang ganja dengan total berat ganja basah mencapai 80 ton.",
         image: "/path/to/anak-anak-image.jpg",
         backgroundColor: "#E33B3B",
         height: "300px",
@@ -31,9 +31,9 @@ export const options = [
         ],
     },
     {
-        title: "Remaja",
+        title: "Merokok",
         description: "13-17 Tahun",
-        information: "loremipsumkan saja",
+        information: "Data terkait merokok di Indonesia menunjukkan bahwa Indonesia adalah salah satu negara dengan prevalensi perokok tertinggi di dunia. Pada 2022, diperkirakan lebih dari 61,4 juta orang di Indonesia adalah perokok, dengan lebih dari 70% di antaranya adalah laki-laki dewasa. Prevalensi merokok pada remaja juga sangat tinggi, dengan sekitar 20% remaja berusia 13-15 tahun tercatat sebagai perokok aktif.",
         image: "/path/to/remaja-image.jpg",
         backgroundColor: "#0E9D75",
         height: "320px",
@@ -46,9 +46,9 @@ export const options = [
         ]
     },
     {
-        title: "Dewasa",
+        title: "Seksual",
         description: "18-59 Tahun",
-        information: "loremipsumkan saja",
+        information: "Berdasarkan Survei Pengalaman Hidup Perempuan Nasional (SPHPN) 2021 dari BPS dan Kementerian Pemberdayaan Perempuan dan Perlindungan Anak (KPPPA), sekitar 1 dari 3 perempuan di Indonesia berusia 15-64 tahun pernah mengalami kekerasan fisik atau seksual sepanjang hidupnya.",
         image: "/path/to/dewasa-image.jpg",
         backgroundColor: "#F36932",
         height: "270px",
@@ -61,12 +61,27 @@ export const options = [
         ]
     },
     {
-        title: "Lansia",
+        title: "Bullying",
         description: "60 tahun keatas",
-        information: "loremipsumkan saja",
+        information: "Data mengenai bullying di Indonesia menunjukkan bahwa praktik ini masih menjadi masalah serius, terutama di kalangan remaja dan pelajar. Menurut survei dari Kementerian Pendidikan, Kebudayaan, Riset, dan Teknologi, serta beberapa lembaga swadaya masyarakat, prevalensi bullying di lingkungan sekolah cukup tinggi.",
         image: "/path/to/lansia-image.jpg",
         backgroundColor: "#16AE79",
         height: "220px",
+        caseImages: [
+            '/kasus.png',
+            '/kasus.png',
+            '/kasus.png',
+            '/kasus.png',
+            '/kasus.png',
+        ]
+    },
+    {
+        title: "Mabok",
+        description: "60 tahun keatas",
+        information: "Menurut survei dari Badan Narkotika Nasional (BNN), ada tren yang mengkhawatirkan mengenai konsumsi alkohol di kalangan remaja. Data menunjukkan bahwa sekitar 14,4% remaja di kota-kota besar pernah mengonsumsi alkohol, dengan sebagian besar melaporkan pernah mengalami mabuk akibat konsumsi berlebihan.",
+        image: "/path/to/lansia-image.jpg",
+        backgroundColor: "#16AE79",
+        height: "320px",
         caseImages: [
             '/kasus.png',
             '/kasus.png',
