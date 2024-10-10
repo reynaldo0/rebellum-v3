933e68d0dc9e0fcb1be42c4448cd4058f7ccef8d
reynaldo0 <reynaldooyusellino@gmail.com>
2024-10-10 09:43:13 +0700

    add : animation tawuran & ganja

--- src/components/models/Ganja.tsx
+++ src/components/models/Ganja.tsx
@@ -1,4 +1,6 @@
+import { useRef, useState } from 'react';
 import { useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 import { GLTF } from 'three-stdlib';
 import * as THREE from 'three';
 
@@ -15,9 +17,28 @@ type GLTFResult = GLTF & {
 
 const Ganja = (props: JSX.IntrinsicElements["group"]) => {
   const { nodes, materials } = useGLTF('/3D/ganja.glb') as GLTFResult;
+  const meshRef = useRef<THREE.Group>(null); // Menggunakan ref untuk grup
+
+  // State untuk melacak rotasi
+  const [rotatedOnce, setRotatedOnce] = useState(false);
+
+  // Animasi rotasi
+  useFrame(() => {
+    if (meshRef.current) {
+      // Jika belum berotasi sekali, lakukan rotasi
+      if (!rotatedOnce) {
+        meshRef.current.rotation.y += 0.01; // Lakukan rotasi
+        // Jika rotasi mencapai 2π (360 derajat), reset kembali
+        if (meshRef.current.rotation.y >= Math.PI * 2) {
+          meshRef.current.rotation.y = 0; // Kembalikan rotasi ke 0
+          setRotatedOnce(true); // Tandai bahwa rotasi sudah dilakukan sekali
+        }
+      }
+    }
+  });
 
   return (
-    <group {...props} dispose={null}>
+    <group {...props} dispose={null} ref={meshRef}>
       <group scale={0.01}>
         {/* Mengubah posisi y menjadi sedikit lebih rendah, misalnya 8.0 */}
         <group position={[0, -7.0, 0]} rotation={[Math.PI / 2, 0, Math.PI]} scale={140}>
